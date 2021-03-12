c684bb31597c3ffa47ae9d2c10a7491e46cccc13
blackkillers20 <77771541+blackkillers20@users.noreply.github.com>
2021-03-12 23:29:41 +0700

    make it better

--- app.js
+++ app.js
@@ -16,26 +16,13 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: false}))
 
 app.get('/', async (req,res)=>{
-    //check session userName if exited
-    myses = req.session;
-    if(myses.userName !=null){   
-        let client= await MongoClient.connect(url, {useUnifiedTopology: true});
+        let client= await MongoClient.connect(url);
         let dbo = client.db("ProductTesting");
         let results = await dbo.collection("ProductTesing").find({}).toArray();
-        res.render('viewproducts',{model:results,userName:myses.userName})
-    }else{
-        res.render('Login')
-    }
-})
-app.get('/viewproducts', async(req, res)=>
-{
-    let client= await MongoClient.connect(url, {useUnifiedTopology: true});
-    let dbo = client.db("ProductTesting");
-    let results = await dbo.collection("ProductTesing").find({}).toArray();
-    res.render('viewproducts', {model: results}) 
+        res.render('viewproducts',{model:results})
 })
 app.get ('/Control_Center',async (req, res)=>{
-    let client= await MongoClient.connect(url, {useUnifiedTopology: true});
+    let client= await MongoClient.connect(url);
     let dbo = client.db('ProductTesting');
     let results = await dbo.collection("ProductTesing").find({}).toArray({});
     res.render('home', {model:results})
@@ -50,45 +37,12 @@ app.get("/delete", async(req, res)=>{
     let id = req.query.id;
     var ObjectID = require("mongodb").ObjectID;
     let condition = {_id: ObjectID(id) };
-    let client= await MongoClient.connect(url, {useUnifiedTopology: true});
+    let client= await MongoClient.connect(url);
     let dbo = client.db("ProductTesting");
     await dbo.collection("ProductTesing").deleteOne(condition);
     let results = await dbo.collection("ProductTesing").find({}).toArray({});
     res.redirect('/')
 });
-app.get('/edit', async(req, res)=>{
-    let id = req.query.pid;
-    var ObjectID = require("mongodb").ObjectID;
-    let condition = {"_id": ObjectID(id)};
-    let client= await MongoClient.connect(url, {useUnifiedTopology: true});
-    let dbo = client.db("ProductTesting");
-    let prod = await dbo.collection("ProductTesing").findOne(condition)
-    res.render('edit', {model: prod});
-})
-app.get('/login', (req, res)=>{
-    res.render('Login')
-})
-app.post('/update', async(req, res)=>{
-    let client= await MongoClient.connect(url, {useUnifiedTopology: true});
-    let dbo = client.db("ProductTesting");
-    let Name = req.body.productName;
-    let Price = req.body.price;
-    let Date = req.body.ImportedDate;
-    let Clothes = req.body.outfit;
-    let ID = req.body.pid;
-    var ObjectID = require('mongodb').ObjectID;
-    let condition = {"_id":ObjectID(ID)};
-    let updateProduct ={$set : {productName : Name, price:Price, ImportedDate: Date, outfit: Clothes}} ;
-    await dbo.collection("ProductTesing").updateOne(condition,updateProduct);
-    res.redirect('/viewproducts');  
-})
-app.post('/search', async(req, res)=>{
-    let searchText = req.body.txtSearch;
-    let client= await MongoClient.connect(url, {useUnifiedTopology: true});
-    let dbo = client.db("ProductTesting");
-    let results = await dbo.collection("ProductTesing"). find({productName: new RegExp(searchText, 'i')}).toArray();
-    res.render('viewproducts',{model: results})
-})
 app.post('/insert', async (req,res)=>{
     let client= await MongoClient.connect(url, {useUnifiedTopology: true});
     let dbo = client.db('ProductTesting'); 
@@ -101,17 +55,6 @@ app.post('/insert', async (req,res)=>{
     await dbo.collection('ProductTesing').insertOne(newProduct);
     
     res.redirect('/viewproducts')  
-})
-app.post('/doLogin',(req,res)=>{
-    let Nameinput = req.body.txtName;
-    let PassInput = req.body.txtPassword;
-    if(Nameinput != 'admin' || PassInput != 'admin'){
-        res.render('Login', {errorMsg: "INCORRECT!!!!!!!!!!1"})
-    }else{
-        myses = req.session;
-        myses.userName = Nameinput;
-        res.redirect('/')
-    }
 })
 
 const PORT = process.env.PORT || 5000
