26b236ef9150612e7cf152b10165d261199333ba
blackkillers20 <77771541+blackkillers20@users.noreply.github.com>
2021-03-13 09:04:52 +0700

    make it better

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.post('/search', async(req, res)=>{
     let searchText = req.body.txtSearch;
     let client= await MongoClient.connect(url, {useUnifiedTopology: true});
     let dbo = client.db("ProductTesting");
-    let results = await dbo.collection("ProductTesing"). find({price: new RegExp(searchText, 'i')}).toArray();
+    let results = await dbo.collection("ProductTesing"). find({price: new RegExp(searchText, 'i', {$gt: 300, $lt: 400})}).toArray();
     res.render('viewproducts',{model: results})
 })
 app.get('/edit', async(req, res)=>{
